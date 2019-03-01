8da2db0f06b489832522b170be7a341975d7df0e
Ryoji Kono <k.ryoji.74@gmail.com>
2019-03-01 12:58:57 +0900

    added functions to send & receive texts :thought_balloon:

--- src/javascripts/App.vue
+++ src/javascripts/App.vue
@@ -1,19 +1,28 @@
 <template>
-  <div>
-    <p>
-      <img class="logo" src="../images/logo.jpg" alt="ロゴ">
-      <span class="sample">サンプルコード</span>
-    </p>
-    <MyComponent :message="$data.message" />
-    <form @submit="onSubmit">
-      <input v-model="$data.text" type="text">
-      <button type="submit">送信</button>
-    </form>
-  </div>
+<div>
+  <p>
+    <img class="logo" src="../images/logo.jpg" alt="ロゴ">
+    <span class="sample">サンプルコード</span>
+  </p>
+  <p style="color: red;" v-if="errorMessage">{{ errorMessage }}</p>
+  <MyComponent :message="$data.message" />
+  <form @submit="onSubmit">
+    <input type="text" v-model="userName">
+    <input v-model="$data.text" type="text" required>
+    <button type="submit">送信</button>
+  </form>
+  <div v-html="identicon"></div>
+  <ul>
+    <li v-for="chat in chats">
+      {{ chat }}
+    </li>
+  </ul>
+</div>
 </template>
 
 <script>
 import socket from './utils/socket';
+const jdenticon = require('jdenticon');
 
 // components
 import MyComponent from './components/MyComponent.vue';
@@ -24,18 +33,36 @@ export default {
   },
   data() {
     return {
+      userName: '',
       message: '',
-      text: ''
+      text: '',
+      chats: [],
+      errorMessage: ''
     };
   },
   created() {
     socket.on('connect', () => {
       console.log('connected!');
+      // チャットログを受け取るためにcreatedでgetChatLogを送信
+      socket.emit('getChatLog', () => {});
+      console.log('getting chat log');
+    });
+
+    socket.on('setChatLog', (messages) => {
+      // サーバーからチャットの配列を受け取って追加
+      this.chats.push(...messages);
+      console.log(messages);
     });
 
     socket.on('send', (message) => {
-      console.log(message);
-      this.$data.message = message;
+      // messageが空欄だった場合にエラーメッセージを表示
+      if (message) {
+        console.log(message);
+        this.$data.message = message;
+        this.chats.push(message);
+      } else {
+        this.errorMessage = '投稿内容を入力してください';
+      }
     });
   },
   methods: {
@@ -45,6 +72,13 @@ export default {
     onSubmit(e) {
       e.preventDefault();
       socket.emit('send', this.$data.text);
+      this.text = '';
+      this.errorMessage = '';
+    }
+  },
+  computed: {
+    identicon() {
+      return jdenticon.toSvg(this.userName, 200);
     }
   }
 };
