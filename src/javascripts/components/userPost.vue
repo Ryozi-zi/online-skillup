60d9ed66a9d83f2bcb8f4e7e8998e87a4f2e16a4
Ryoji Kono <b1600579@gl.aiu.ac.jp>
2019-03-01 16:07:40 +0900

    Rename the component and fixed template

---
+++ src/javascripts/components/userPost.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <ul id="chats">
+    <li v-for="chat in chatLog">
+      <h2>{{ chat.username }}</h2>
+      <p> {{ chat.text }}</p>
+      <div v-html="identicon(chat.username)"></div>
+    </li>
+  </ul>
+</template>
+
+<script>
+const jdenticon = require('jdenticon');
+
+export default {
+  props: {
+    chatLog: Array
+  },
+  methods: {
+    identicon: (username) => {
+      return jdenticon.toSvg(username, 50);
+    },
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+li {
+  list-style: none;
+}
+</style>
