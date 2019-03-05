4b1075555e425c6e4a2a449cf753c408e58de7df
Ryoji Kono <b1600579@gl.aiu.ac.jp>
2019-03-05 20:59:59 +0900

    separate a list of messages from App.vue to userPost.vue component

--- src/javascripts/App.vue
+++ src/javascripts/App.vue
@@ -5,14 +5,14 @@
     <span class="sample">サンプルコード</span>
   </p>
   <p style="color: red;" v-if="errorMessage">{{ errorMessage }}</p>
+  <userPost :chatLog="chatLog" id="log"></userPost>
   <form @submit="onSubmit">
     <label for="name">Username</label>
     <input type="text" name="username" v-model="userName" placeholder="ユーザーネーム" required>
     <label for="text">Posts</label>
-    <input v-model="$data.text" name="text" type="text" placeholder="投稿" required>
+    <textarea v-model="$data.text" name="text" type="text" placeholder="投稿" required @keyup.ctrl.enter="onSubmit"></textarea>
     <button type="submit">送信</button>
   </form>
-    <userPost :chatLog="chatLog"></userPost>
 </div>
 </template>
 
@@ -47,7 +47,6 @@ export default {
     socket.on('setChatLog', (messages) => {
       // サーバーからチャットの配列を受け取って追加
       this.chatLog.push(...messages);
-      console.log(messages);
     });
     // 返ってきたメッセージとユーザーネームのobjectを自身のchatLogに代入
     socket.on('send', (object) => {
@@ -63,6 +62,8 @@ export default {
       e.preventDefault();
       // textとusernameが空かどうか判定する
       if (this.text && this.$data.userName) {
+        // 送信時に改行のデーターをhtmlの<br>に変換
+        this.text = this.text.replace(/\n/g, '<br>');
         socket.emit('send', this.$data.text, this.$data.userName);
         this.errorMessage = '';
       } else {
@@ -70,13 +71,6 @@ export default {
         this.errorMessage = '投稿内容、もしくはユーザーネームを入力してください';
       }
       this.text = '';
-    },
-
-    scroll() {
-      const childElement = document.getElementById('chats').lastChild;
-      if (childElement) {
-        childElement.scrollIntoView(false);
-      }
     }
   }
 };
@@ -87,7 +81,19 @@ export default {
   width: 40px;
 }
 
+#log {
+  height: 85vh;
+  width: 80%;
+  overflow: auto;
+}
+
 .sample {
   color: $red;
+}
+
+form {
+  position: fixed;
+  bottom: 0;
+  left: 0;
 }
 </style>
